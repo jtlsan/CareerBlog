dda075b4c2bb074b62e7366a36a66e6ec772e005
San <jtlsan@naver.com>
2019-05-18 00:45:22 +0900

    finished mobile-first

--- home/static/js/post_list.js
+++ home/static/js/post_list.js
@@ -1,10 +1,6 @@
 (function(win, $){
-    $(document).ready(function() {
-        $(".grid-post li").on("click", function(){
-            var next_url = $(this).children().eq(1).attr("href");
-            location.href = next_url;
-        })
-        $.each($(".grid-post li"), function(){
+    $.categoryColor = function() {
+        $.each($("li.list-raw"), function() {
             var txt = $(this).children("#category").text();
             if (txt == "Study") {
                 $(this).children("#category").css("background-color", "#D5FBCC");
@@ -15,6 +11,41 @@
             else {
                 $(this).children("#category").css("background-color", "#FBCCD5");
             }
+            $(this).removeClass("list-raw");
         });
+    }
+    $(function() {
+        $.categoryColor();
     })
+    $(document).on('click', ".grid-post li", function() {
+        var next_url = $(this).children().eq(1).attr("href");
+        location.href = next_url;
+    });
+    $(document).on("DOMNodeInserted", ".grid-post", function() {
+        $.categoryColor();
+    });
+
+
+
+    
+    /*
+    $("li.list").ready(function() {
+        $(".grid-post li").on("click", function(){
+            var next_url = $(this).children().eq(1).attr("href");
+            location.href = next_url;
+        })
+        $.each($(".grid-post li"), function(){
+            var txt = $(this).children("#category").text();
+            if (txt == "Study") {
+                $(this).children("#category").css("background-color", "#D5FBCC");
+            }
+            else if (txt == "Projects") {
+                $(this).children("#category").addClass("Projects");
+            }
+            else {
+                $(this).children("#category").addClass("Articles");
+            }
+        }); 
+    });
+    */
 }(window, jQuery));
--- home/views.py
+++ home/views.py
@@ -4,40 +4,84 @@ from home.forms import PostForm, LoginForm
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 # Create your views here.
 
 def home(request):
     posts = Post.objects.all()
     title = "전체 포스팅"
+    page = request.GET.get('page', 1)
+    paginator = Paginator(posts, 5)
+    post_count = posts.count()
+    try:
+        post_list = paginator.page(page)
+    except PageNotAnInteger:   
+        post_list = paginator.page(1)
+    except EmptyPage:
+        post_list = paginator.page(paginator.num_pages)
     return render(request, "home/home.html", {
-        'posts': posts,
+        'post_count': post_count,
         "title": title,
+        'post_list': post_list
         })
 
 def projects(request):
     posts = Post.objects.all().filter(category="Projects")
     title = "Projects"
+    page = request.GET.get('page', 1)
+    paginator = Paginator(posts, 5)
+    post_count = posts.count()
+    try:
+        post_list = paginator.page(page)
+    except PageNotAnInteger:
+        post_list = paginator.page(1)
+    except EmptyPage:
+        post_list = paginator.page(paginator.num_pages)
     return render(request, "home/home.html", {
-        'posts': posts,
+        'post_count': post_count,
         "title": title,
+        'post_list': post_list,
     })
 
 def study(request):
     posts = Post.objects.all().filter(category="Study")
     title = "Study"
+    page = request.GET.get('page', 1)
+    paginator = Paginator(posts, 5)
+    post_count = posts.count()
+    try:
+        post_list = paginator.page(page)
+    except PageNotAnInteger:
+        post_list = paginator.page(1)
+    except EmptyPage :
+        post_list = paginator.page(paginator.num_pages)
     return render(request, "home/home.html", {
-        'posts': posts,
+        'post_count': post_count,
         'title': title,
+        'post_list': post_list,
     })
 
 def articles(request):
     posts = Post.objects.all().filter(category="Article")
     title = "Articles"
+    page = request.GET.get('page', 1)
+    paginator = Paginator(posts, 5)
+    post_count = posts.count()
+    try :
+        post_list = paginator.page(page)
+    except PageNotAnInteger:
+        post_list = paginator.page(1)
+    except EmptyPage:
+        post_list = paginator.page(paginator.num_pages)
     return render(request, "home/home.html", {
-        'posts': posts,
+        'post_count': post_count,
         'title': title,
+        'post_list': post_list,
     })
 
+@login_required
 def post_create(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -54,6 +98,7 @@ def post_detail(request, slug_url):
     post = get_object_or_404(Post, slug=slug_url)
     return render(request, "home/post_detail.html", {'post': post})
 
+@login_required
 def post_update(request, slug_url):
     post = get_object_or_404(Post, slug=slug_url)
     if request.method == "POST":
@@ -67,6 +112,7 @@ def post_update(request, slug_url):
         form = PostForm(instance=post)
         return render(request, "home/post_create.html", {'form': form})
 
+@login_required
 def post_delete(request, slug_url):
     post = get_object_or_404(Post, slug=slug_url)
     post.delete()
